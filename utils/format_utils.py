daa037f26c5c58cd6c6c7073d76d0dcc0aa9464b
karanversation <karanbajaj23@gmail.com>
2022-04-21 23:29:34 +0530

    reducing boundary len, fixing odd len

--- utils/format_utils.py
+++ utils/format_utils.py
@@ -1,13 +1,12 @@
 
-DEFAULT_LENGTH = 100
+DEFAULT_LENGTH = 81
 
 def print_title_boundary(title, char='*', length=DEFAULT_LENGTH):
-    if not title:
-        print char * length
-        return
-    part_len = (length-len(title)-2)/2
-    final_str = char * part_len + ' {} '.format(title) + char * part_len
-    if len(final_str)%2 == 1:
+    final_str = char * length
+    if title:
+        part_len = (length-len(title)-2)/2
+        final_str = char * part_len + ' {} '.format(title) + char * part_len
+    if len(final_str) < length:
         final_str = final_str + char
     print final_str
 
